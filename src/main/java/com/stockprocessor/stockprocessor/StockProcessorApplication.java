424fd30c357ff6e4a1467180e09a49567a4ed532
Rodney Bochie <ace@MacBook-Pro.local>
2024-06-20 06:01:57 +0000

    first commit

---
+++ src/main/java/com/stockprocessor/stockprocessor/StockProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.stockprocessor.stockprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockprocessor/stockprocessor/StockProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockprocessor.stockprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
